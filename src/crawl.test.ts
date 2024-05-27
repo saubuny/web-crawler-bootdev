8668435135544e139728abc440dcef614609261d
saubuny <saubuny@gmail.com>
2024-05-27 08:00:55 -0400

    prog: url processing

---
+++ src/crawl.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "bun:test";
+import { normalizeUrl, getUrlFromHtml } from "./crawl";
+
+test("normalizeUrl", () => {
+	expect(normalizeUrl("https://blog.boot.dev/path/")).toBe(
+		"blog.boot.dev/path",
+	);
+	expect(normalizeUrl("https://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+	expect(normalizeUrl("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
+	expect(normalizeUrl("http://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+});
+
+test("getUrlFromHtml", () => {
+	const testHtml1 = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+	const testHtml2 = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path"><span>Go to Boot.dev</span></a>
+            <a href="https://blog.boot.dev/path"><span>Go to Boot.dev</span></a>
+            <a href="https://blog.boot.dev/path/path1"><span>Go to Boot.dev</span></a>
+            <a href="/path/path2"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>
+    `;
+
+	expect(getUrlFromHtml(testHtml1, "blog.boot.dev")).toEqual(["blog.boot.dev"]);
+	expect(getUrlFromHtml(testHtml2, "blog.boot.dev")).toEqual([
+		"blog.boot.dev/path",
+		"blog.boot.dev/path",
+		"blog.boot.dev/path/path1",
+		"blog.boot.dev/path/path2",
+	]);
+});
---
+++ src/crawl.ts
@@ -0,0 +1,26 @@
+import { JSDOM } from "jsdom";
+
+function removeTrailingSlash(str: string): string {
+	if (str.endsWith("/")) return str.substring(0, str.length - 1);
+	return str;
+}
+
+function normalizeUrl(urlStr: string): string {
+	const url = new URL(urlStr);
+	return url.hostname + removeTrailingSlash(url.pathname);
+}
+
+function getUrlFromHtml(htmlBody: string, baseUrl: string): string[] {
+	const jsdom = new JSDOM(htmlBody);
+	const anchors = jsdom.window.document.querySelectorAll("a");
+	const urls: string[] = [];
+	anchors.forEach((a) => {
+		// Weird quirk where a.pathname does not include the pathname
+		if (a.href.includes(baseUrl))
+			urls.push(removeTrailingSlash(baseUrl + a.pathname));
+		else urls.push(removeTrailingSlash(baseUrl + a.href));
+	});
+	return urls;
+}
+
+export { normalizeUrl, getUrlFromHtml };
