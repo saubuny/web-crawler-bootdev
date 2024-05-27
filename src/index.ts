6a855aa44da7e84b283711ea94619cbcb1fa1b22
saubuny <saubuny@gmail.com>
2024-05-27 08:17:00 -0400

    prog: cli parsing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+function main() {
+	if (Bun.argv.length < 3) {
+		console.error("[Error] No given base URL");
+		return;
+	}
+	if (Bun.argv.length > 3) {
+		console.error("[Error] Too many arguments");
+		return;
+	}
+
+	const baseUrl = Bun.argv[2];
+	console.log(baseUrl);
+}
+
+main();
+
